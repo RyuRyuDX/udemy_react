3888abd4b63cb2a09166ebf59b1926cb70e02698
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2021-03-23 02:06:00 +0900

    redux increment decrement

---
+++ src/actions/index.js
@@ -0,0 +1,12 @@
+const { type } = require("os");
+
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: 'INCREMENT'
+})
+
+export const decrement = () => ({
+  type: 'DECREMENT'
+})
