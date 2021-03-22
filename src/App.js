538c916d52ef16e125e12787c3be729ff8d223ab
Ryusuke Onose <gibson.red.gin.suke@gmail.com>
2021-03-23 01:25:57 +0900

    something

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
+import PropTypes from 'props-types';
 
 const App = () => {
+const profiles = [
+  { name: "Taro", age: 10 },
+  { name: "Onose", age: 26 },
+  { name: 1 },
+]
   return (
     <div>
-      <User name={"Taro"} age={10} />
-      <User name={"Ryusuke"} age={26} />
+      {
+        profiles.map((profile, index) => {
+          return <User name={profile.name} age={profile.age} key={index} />
+        })
+      }
     </div>
   )
 }
@@ -13,6 +22,8 @@ const User = (props) => {
   return <div>Hi, I am {props.name} and {props.age} </div>
 }
 
-
+User.propsTypes = {
+  name: PropTypes.string
+}
 
 export default App;
